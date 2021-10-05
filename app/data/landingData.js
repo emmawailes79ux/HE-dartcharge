1f66fbf59e81e5698273595f1bfa911932f50703
Chitransh Srivastava <chitransh.srivastava@conduent.com>
2021-10-05 12:04:28 +0530

    Initial commit

---
+++ app/data/landingData.js
@@ -0,0 +1,41 @@
+exports.chargeListHead = [
+  "Type of vehicle",
+  "One-off payment",
+  "PayG Service",
+  "Pre-pay Accounts",
+];
+
+exports.chargeList = [
+  {
+    title: "Motorcycle, mopeds, quad bikes",
+    oneOff: "Free",
+    payG: "Free",
+    prePay: "Free",
+  },
+  {
+    title: "Cars, motorhomes, minibuses",
+    oneOff: "£2.50",
+    payG: "£2.50",
+    prePay: "£2.00",
+  },
+  {
+    title: "Vehicles with 2 axles",
+    oneOff: "£3.00",
+    payG: "£3.00",
+    prePay: "£2.63",
+  },
+  {
+    title: "Vehicles with more than 2 axles",
+    oneOff: "£6.00 ",
+    payG: "£6.00 ",
+    prePay: "£5.19 ",
+  },
+];
+
+exports.vehicleList = [
+  { 'head':"Registration Number", 'value':'AN23CHA'},
+   {'head':"Class", 'value': 'B'},
+   {'head': "Make", 'value':'AUDI' },
+   {'head': "Model", 'value':'TT FSI' },
+   {'head': "Colour", 'value':'Black' },
+ ];
